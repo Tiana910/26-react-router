a377b7d547f20c4dc6a3160184e3112e1eac7f3b
Tiana910 <tatsiana.magonova@gmail.com>
2023-03-06 23:59:54 +0100

    React Router

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Contacts = () => {
+  return (
+    <>
+      <h1>Contacts</h1>
+    </>
+  );
+};
+
+export default Contacts;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from 'react-router-dom';
+
+const Menu = () => {
+  return (
+    <nav>
+      <NavLink
+        //className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+        // style={({ isActive }) => (isActive ? { color: 'lightgreen' } : {})}
+        to="."
+        end
+      >
+        Home
+      </NavLink>
+      <NavLink
+        // className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+        // style={({ isActive }) => (isActive ? { color: 'lightgreen' } : {})}
+        to="/about"
+      >
+        About
+      </NavLink>
+      <NavLink
+        //className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+        // style={({ isActive }) => (isActive ? { color: 'lightgreen' } : {})}
+        to="/contacts"
+      >
+        Contacts
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Menu;
